b625a3bc3b8ed0f5e7f85462136c067e295442c9
robbepop <robbepop@web.de>
2016-03-29 12:53:53 +0200

    hopefully fixed coveralls.io

--- src/utils/in_range_of.rs
+++ src/utils/in_range_of.rs
@@ -8,3 +8,24 @@ impl<T> InRangeOf<T> for T where T: PartialEq + PartialOrd {
 		lower_bound <= *self && *self <= upper_bound
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test() {
+		assert!( 1.in_range_of(1,3));
+		assert!( 2.in_range_of(1,3));
+		assert!( 3.in_range_of(1,3));
+		assert!(!4.in_range_of(1,3));
+		assert!('b'.in_range_of('a', 'z'));
+		assert!('B'.in_range_of('A', 'Z'));
+	}
+
+	#[test]
+	#[should_panic]
+	fn invalid_lower_upper() {
+		assert!('c'.in_range_of('z', 'a'));
+	}
+}
