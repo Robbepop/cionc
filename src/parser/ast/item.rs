7d989540c48fb1ff1fe58a4a7b1119e8133935d2
Robbepop <robbepop@web.de>
2017-01-29 03:57:42 +0100

    further extended ast definitions, especially item and slightly cleaned up stmt

--- src/parser/ast/item.rs
+++ src/parser/ast/item.rs
@@ -1,15 +1,74 @@
-use ::string_interner::{Name};
+use ast::node_id::NodeId;
 use ast::ptr::P;
+use ast::expr::Expr;
+
+use ::string_interner::{Name};
 
 pub struct Item {
-	name: Name,
+	id  : NodeId,
 	kind: ItemKind
 }
 
 pub enum ItemKind {
-	Module(ModuleItem),
+	Decl(Decl),
+	Use(UseItem),
+	Impl(ImplItem)
 }
 
-pub struct ModuleItem {
-	items: Vec<P<Item>>
+pub struct UseItem {
+	// TODO
+}
+
+pub struct ImplItem {
+	// TODO
+}
+
+pub struct Decl {
+	name: Name,
+	kind: DeclKind,
+	visibility: Visibility
+}
+
+pub enum Visibility {
+	Unspec,
+	Pub,
+}
+
+pub enum DeclKind {
+	Expr(ExprDecl),
+	Fn(FnDecl),
+	Struct(StructDecl),
+	Enum(EnumDecl),
+	Trait(TraitDecl),
+	Module(ModuleDecl)
+}
+
+pub struct ExprDecl {
+	expr: P<Expr>,
+	mutability: Mutability
+}
+
+pub enum Mutability {
+	Immutable,
+	Mutable
+}
+
+pub struct FnDecl {
+	// TODO
+}
+
+pub struct StructDecl {
+	// TODO
+}
+
+pub struct EnumDecl {
+	// TODO
+}
+
+pub struct TraitDecl {
+	// TODO
+}
+
+pub struct ModuleDecl {
+	items: Vec<Item>
 }
--- src/parser/ast/stmt.rs
+++ src/parser/ast/stmt.rs
@@ -1,18 +1,15 @@
+use ast::node_id::NodeId;
 use ast::ptr::P;
 use ast::expr::Expr;
 use ast::item::Item;
 
 pub struct Stmt {
+	id  : NodeId,
 	kind: StmtKind
 }
 
 pub enum StmtKind {
-	Let(LetStmt),
 	Item(P<Item>),
 	Expr(P<Expr>),
 	Semi(P<Expr>)
-}
-
-pub struct LetStmt {
-
 }
